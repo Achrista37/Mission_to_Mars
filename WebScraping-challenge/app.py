6bd948a00bf3c86052347f983858ae8282979405
andriani <a.christanty@gmail.com>
2022-03-18 23:57:17 +0800

    add

---
+++ WebScraping-challenge/app.py
@@ -0,0 +1,44 @@
+
+
+#################################################
+# Database Setup
+#################################################
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+# Create an instance of Flask
+app = Flask(__name__)
+
+# Use PyMongo to establish Mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Route to render index.html template using data from Mongo
+@app.route("/")
+def home():
+
+    # Find one record of data from the mongo database
+    mars_db = mongo.db.mars_db.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars_db=mars_db)
+
+
+# Route that will trigger the scrape function
+@app.route("/scrape")
+def scrape():
+
+    # Run the scrape function
+    mars_db = mongo.db.mars_db
+    mars_data = scrape_mars.scrape()
+
+    # Update the Mongo database using update and upsert=True
+    mars_db.update_one({}, {"$set": mars_data}, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
